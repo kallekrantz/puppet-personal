82a9fcf4e79417b6f1e224a8e19cf8a66a6c0b52
Karl Johan Krantz <schwomp@gmail.com>
2013-09-06 14:51:24 +0200

    Displaymanager configuration, the shit?

---
+++ modules/software/manifests/displaymanager.pp
@@ -0,0 +1,23 @@
+class software::displaymanager{
+  require aptconf
+  $packages = ["gnome-shell",
+               "ubuntu-gnome-desktop",
+               "gnome-panel",
+               "xmonad",
+               "gdm"]
+  package{$packages:}
+  
+  service{"gdm":
+    ensure => running,
+    require => [Package['ubuntu-gnome-desktop'], Package['gdm']]
+  }
+  file{'/usr/share/gnome-session/sessions/xmonad.session':
+    source => "puppet:///modules/software/xmonad/xmonad.session"
+  }
+  file{'/usr/share/xsessions/xmonad-gnome-session.desktop':
+    source => "puppet:///modules/software/xmonad/xmonad-gnome-session.desktop"
+  }
+  file{'/usr/share/applications/xmonad.desktop':
+    source => "puppet:///modules/software/xmonad/applications/xmonad.desktop"
+  }
+}
