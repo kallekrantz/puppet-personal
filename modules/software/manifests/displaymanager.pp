dbf3bc59e6984fe5d1c30fdcf97dc6e4db013d57
Karl Johan Krantz <schwomp@gmail.com>
2013-09-09 18:00:57 +0200

    Some more fix with lightdm and gdm, I hate lightdm, curse it

--- modules/software/manifests/displaymanager.pp
+++ modules/software/manifests/displaymanager.pp
@@ -7,22 +7,16 @@ class software::displaymanager{
                "gdm"]
   package {$packages:}
   
-  service {"gdm":
-    ensure => running,
-    enable => true,
-    require => [Package['ubuntu-gnome-desktop'], Package['gdm']]
-  }
-  service {"lightdm":
-    enable => false,
-    ensure => stopped
-  }
   file {'/usr/share/gnome-session/sessions/xmonad.session':
-    source => "puppet:///modules/software/xmonad/xmonad.session"
+    source => "puppet:///modules/software/displaymanager/xmonad.session"
   }
   file {'/usr/share/xsessions/xmonad-gnome-session.desktop':
-    source => "puppet:///modules/software/xmonad/xmonad-gnome-session.desktop"
+    source => "puppet:///modules/software/displaymanager/xmonad-gnome-session.desktop"
   }
   file {'/usr/share/applications/xmonad.desktop':
-    source => "puppet:///modules/software/xmonad/applications/xmonad.desktop"
+    source => "puppet:///modules/software/displaymanager/xmonad/applications/xmonad.desktop"
+  }
+  file {'/etc/X11/default-display-manager':
+    source => "puppet:///modules/software/displaymanager/default-display-manager"
   }
 }
